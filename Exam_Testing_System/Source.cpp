211dae5cf94a3ee39ec463298c8e33e6576aa199
iVan roSHka <ivroshmail@gmail.com>
2021-09-23 09:16:00 +0300

    23.09.2021

--- Exam_Testing_System/Source.cpp
+++ Exam_Testing_System/Source.cpp
@@ -5,39 +5,4 @@ int main() {
 	SetConsoleOutputCP(866);
 	Account acc;
 	acc.menu();
-	
-	 
-	/*Theme t("Math");
-
-	t.insert_question("2+2=?");
-	t.questions[0].insert_answer("11", 0);
-	t.questions[0].insert_answer("1", 0);
-	t.questions[0].insert_answer("4", 1);
-	t.questions[0].insert_answer("3", 0);
-
-	t.insert_question("99-100=?");
-	t.questions[1].insert_answer("-1", 1);
-	t.questions[1].insert_answer("199", 0);
-	t.questions[1].insert_answer("13", 0);
-	t.questions[1].insert_answer("-17", 0);
-	t.questions[1].insert_answer("-17", 0);
-
-	t.insert_question("10/2=5?");
-	t.questions[2].insert_answer("Да", 1);
-	t.questions[2].insert_answer("Нет", 0);
-
-	t.insert_question("44+?=100");
-	t.questions[3].insert_answer("64", 0);
-	t.questions[3].insert_answer("78", 0);
-	t.questions[3].insert_answer("66", 0);
-	t.questions[3].insert_answer("56", 1);
-
-
-
-	map<int,Menu> qMenu, aMenu;
-	map<int,vector<string>>QuestionsMenu, AnswersMenu;*/
-
-
-		
-
 }
